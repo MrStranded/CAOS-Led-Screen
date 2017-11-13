3d718d21cca7666c39a099ef3f3910390619ca55
Michael <m.pluess@stud.unibas.ch>
2017-11-13 11:55:03 +0100

    implemented some ledInterface methods

--- src/main/ledInterface.ino
+++ src/main/ledInterface.ino
@@ -4,6 +4,20 @@
 //
 // -----------------------------------------------------------------------------
 
+// this array holds the data of the pixels
+// each char corresponds to a column of the screen
+// DO NOT access pixels[] outside of this file!
+
+char[] pixels = new char[48];
+
+// %%%%%%%%%%%%%%%%%%%%%
+
+// initializes the screen
+
+void initScreen() {
+  clear();
+}
+
 // %%%%%%%%%%%%%%%%%%%%%
 
 // writes given char into specified position on screen
@@ -19,7 +33,29 @@ void writeChar(char c, int position) {
 // power = 0 <=> led is off
 
 void setPixel(int x,int y,int power) {
-  // TODO
+  // pixelMask is 0 everywhere, except in position y
+  char pixelMask = 1 << (y-1);
+  
+  // columnMask is 1 everywhere, except in position y
+  char columnMask = 255 - pixelMask;
+  
+  // pixelMask has to be multiplied with power, so it contains new value of pixel
+  pixelMask = pixelMask * power;
+  
+  // the pixelvalue copied at every bit where pixelMask is 1 and reset at the position where pixelMask is 0 by columnMask
+  pixels[x] = (pixels[x] & pixelMask) | columnMask;
+}
+
+// %%%%%%%%%%%%%%%%%%%%%
+
+// gets the power mode of the specified pixel
+
+int getPixel(int x,int y) {
+  // mask is 0 everywhere, except in position y
+  char mask = 1 << (y-1);
+
+  // the & operator returns an int that is 0 everywhere, except in position y, where it has the value of the pixel
+  return pixels[x] & mask;
 }
 
 // %%%%%%%%%%%%%%%%%%%%%
@@ -37,10 +73,14 @@ void clear() {
 // power = 0 <=> leds are off
 
 void setAll(int power) {
+  // c = 0000 0000
+  char c = 0;
+  
+  // c = 1111 1111
+  if (power == 1) { c = 255; }
+  
   for (int x=0; x<48; x++) {
-    for (int y=0; y<8; y++) {
-      setPixel(x,y,power);
-    }
+    pixels[x] = c;
   }
 }
 
