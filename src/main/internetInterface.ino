29f8769eb756af5740538973aaef329ca0c80c68
Michael <m.pluess@stud.unibas.ch>
2017-11-29 18:52:21 +0100

    somehow this file was not staged before

--- src/main/internetInterface.ino
+++ src/main/internetInterface.ino
@@ -1,18 +1,13 @@
-/*
-  Web client
-
- This sketch connects to a website (http://www.google.com)
- using an Arduino Wiznet Ethernet shield.
-
- Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
-
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe, based on work by Adrian McEwen
-
- */
+// -----------------------------------------------------------------------------
+//
+// In this file you will find methods to control the internet connection of the arduino
+// and to get data from the internet conveniantly.
+//
+// If you have trouble with the library files go to:
+// Sketch -> Include Library -> Manage Libraries
+// and look for the Arduino Http Client and install it.
+//
+// -----------------------------------------------------------------------------
 
 #include <SPI.h>
 #include <Ethernet.h>
@@ -20,10 +15,11 @@
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
-//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-char server[] = "www.google.com";    // name address for Google (using DNS)
+//IPAddress server(74,125,232,128);  // example of numeric IP for Google (no DNS)
+char server[] = "https://www.continentconquest.ch";
 
 // Set the static IP address to use if the DHCP fails to assign
 IPAddress ip(192, 168, 0, 177);
@@ -31,54 +27,61 @@ IPAddress ip(192, 168, 0, 177);
 // Initialize the Ethernet client library
 // with the IP address and port of the server
 // that you want to connect to (port 80 is default for HTTP):
-EthernetClient clientTest;
+EthernetClient client;
 
-void setupINTERNETTest() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+// %%%%%%%%%%%%%%%%%%%%%
 
+// this method sets up a connection to the given url
+// you may read the result with printInternetResponse()
+
+void setupInternetConnection() {
+  
   // start the Ethernet connection:
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
     // try to congifure using IP address instead of DHCP:
     Ethernet.begin(mac, ip);
   }
+  
   // give the Ethernet shield a second to initialize:
   delay(1000);
   Serial.println("connecting...");
 
   // if you get a connection, report back via serial:
-  if (clientTest.connect(server, 80)) {
+  if (client.connect(server, 80)) {
     Serial.println("connected");
     // Make a HTTP request:
-    clientTest.println("GET /search?q=arduino HTTP/1.1");
-    clientTest.println("Host: www.google.com");
-    clientTest.println("Connection: close");
-    clientTest.println();
+    client.println("GET /CAOS/htmltest.html HTTP/1.1");
+    client.println("Host: continentconquest.ch");
+    client.println("Accept: text/html");
+    client.println("Connection: close");
+    client.println();
   } else {
     // if you didn't get a connection to the server:
     Serial.println("connection failed");
   }
 }
 
-void loopINTERNETTest() {
+// %%%%%%%%%%%%%%%%%%%%%
+
+// prints the response from setupInternetConnection onto the Serial output
+
+void printInternetResponse() {
   // if there are incoming bytes available
   // from the server, read them and print them:
-  if (clientTest.available()) {
-    char c = clientTest.read();
+  if (client.available()) {
+    char c = client.read();
     Serial.print(c);
   }
 
   // if the server's disconnected, stop the client:
-  if (!clientTest.connected()) {
+  if (!client.connected()) {
     Serial.println();
     Serial.println("disconnecting.");
-    clientTest.stop();
+    client.stop();
 
     // do nothing forevermore:
     while (true);
   }
 }
+
