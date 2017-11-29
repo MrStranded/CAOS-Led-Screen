479fc5815287753d4e882f521be9de203a28170e
Michael <m.pluess@stud.unibas.ch>
2017-11-29 17:57:28 +0100

    a very basic internet test directly from the interwebzes

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,10 +1,16 @@
 // %%%%%%%%%%%%%%%%%%%%%
 
+// setup of the Serial port used for debugging
+
+void setupSerial() {
+  Serial.begin(9600);
+  Serial.flush();
+}
+
 // main entry point of the code
 
 void setup() {
-  Serial.begin(9600);
-  Serial.flush();
+  setupINTERNETTest();
 }
 
 // %%%%%%%%%%%%%%%%%%%%%
@@ -12,18 +18,7 @@ void setup() {
 // main loop of the program
 
 void loop() {
-
-  initInternet();
-
-  char* url = (char *) "https://www.continentconquest.ch/CAOS/test.txt";
-  Serial.println("connecting to:");
-  Serial.println(url);
-  createConnection(url);
-
-  printInternetResponse();
-
-  // ending the programm
-  while(1) {}
+  loopINTERNETTest();
 }
 
 // %%%%%%%%%%%%%%%%%%%%%
