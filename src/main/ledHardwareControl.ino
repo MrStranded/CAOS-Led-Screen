c7152b91a759512568e9ca06b129c7ccddbec1f5
Lukas Stöckli <lukas.stoeckli@stud.unibas.ch>
2018-01-27 18:46:44 +0100

    cleaned hardware control

--- src/main/ledHardwareControl.ino
+++ src/main/ledHardwareControl.ino
@@ -5,25 +5,31 @@
 //
 // -----------------------------------------------------------------------------
 
-#include <SPI.h>
+// %%%%%%%%%%%%%%%%%%%%%
 
-// pins 1 to 8 : row 1 to 8
+// PIN CONSTANTS FOR LED MATRIX
+// 0/1 can't be used due to serial issues
+// 4 is used for SD card reader
+// 10-14 are used by SPI (ethernet)
+
+// pins connected to rows of LED matrix
 int rowPin[8] = {A5,A4,2,3,A0,5,6,7};
-
-//Pin connected to ST_CP of 74HC595
+// Shift register (74HC595) pins
+//Pin connected to ST_CP
 int latchPin = A1;
-//Pin connected to SH_CP of 74HC595
+//Pin connected to SH_CP
 int clockPin = 8;
-////Pin connected to DS of 74HC595
+////Pin connected to DS
 int dataPin = 9;
 
 // data array for shiftregisters
+// holds data for led row in 6 bytes in ascending order
 byte data[6];
 
 // %%%%%%%%%%%%%%%%%%%%%
 
-// initializes led screen pins
-
+// initialize LED matrix
+// set all LEDs to LOW
 void initLedScreen() {
   for (int i=0; i<8; i++) {
     pinMode(rowPin[i], OUTPUT);
@@ -32,21 +38,11 @@ void initLedScreen() {
   pinMode(dataPin, OUTPUT);
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
-
-  //SPI.begin();
-
-  data[0] = 0b10101010;
-  data[1] = 0b01010101;
-  data[2] = 0b10101010;
-  data[3] = 0b01010101;
-  data[4] = 0b10101010;
-  data[5] = 0b01010101;
 }
 
 // %%%%%%%%%%%%%%%%%%%%%
 
 // draws the screen row by row
-
 void drawLedScreen() {
   // for each row
   for (int r = 0; r < 8; r++) {
@@ -61,54 +57,40 @@ void drawLedScreen() {
     
     // shift out 6 bytes of data
     for (int i = 5; i >= 0; i--) {
-      //SPI.transfer(data[i]);
       shiftOut(data[i]);
     }
     
     // set current row to high
     digitalWrite(rowPin[r], HIGH);
     // latchpin no longer needs to listen for information
+    // set shift register output to shifted data
     digitalWrite(latchPin, HIGH);
-    // display current frame [ms]
+    // display current row [ms]
     delay(1);
     digitalWrite(rowPin[r], LOW);
   }
 }
 
+// %%%%%%%%%%%%%%%%%%%%%
 
-
+// shift out 8 bits in register
+// clock idles low
 void shiftOut(byte data) {
-  // This shifts 8 bits out MSB first, 
-  //on the rising edge of the clock,
-  //clock idles low
 
-  //internal function setup
-  int i=0;
-  int pinState;
-  pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
-
-  //for each bit in the byte myDataOut�
-  //NOTICE THAT WE ARE COUNTING DOWN in our for loop
-  //This means that %00000001 or "1" will go through such
-  //that it will be pin Q0 that lights. 
-  for (i=7; i>=0; i--)  {
+  // shift out each bit in reverse order so the last bit
+  // will light the pin Q0 
+  for (int i=7; i>=0; i--)  {
     digitalWrite(clockPin, 0);
 
-    //if the value passed to myDataOut and a bitmask result 
-    // true then... so if we are at i=6 and our value is
-    // %11010100 it would the code compares it to %01000000 
-    // and proceeds to set pinState to 1.
+    // compare data value to bitmap of position
     if ( data & (1<<i) ) {
-      pinState= 1;
+      digitalWrite(dataPin, 1);
     }
     else {  
-      pinState= 0;
+      digitalWrite(dataPin, 0);
     }
 
-    //Sets the pin to HIGH or LOW depending on pinState
-    digitalWrite(dataPin, pinState);
-    //register shifts bits on upstroke of clock pin  
+    //register shifts bits on rising edge of clock pin  
     digitalWrite(clockPin, 1);
     //zero the data pin after shift to prevent bleed through
     digitalWrite(dataPin, 0);
