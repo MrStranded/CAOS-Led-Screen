d5bb378688c2cf7e6cbc5c20e0ac3fb6bf7f1917
Manuel Rickli <manuel.rickli@stud.unibas.ch>
2018-01-28 23:41:06 +0100

    gg

--- Http Request Handler/requestTest.py
+++ Http Request Handler/requestTest.py
@@ -5,6 +5,16 @@ apiKey = "3410a8375afbfb13baeeff03f2472b6b"
 
 r = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Basel&APPID=" + apiKey)
 
-json.loads(r.text)
+j = json.loads(r.text)
 
-r = requests.get("http://192.168.178.42/?text=yeahboy")
+print(j)
+f = float(j["main"]["temp_min"] - 273.15)
+city = j["name"]
+
+a = int(f)
+b = (f - a)*100
+s = "http://192.168.178.42/?text=Temperatur%20in%20" + city + "%3A%20" + str(a) + "%2E" + str(int(b)) + "%C2%B0C"
+
+print(s)
+
+r = requests.get(s)
