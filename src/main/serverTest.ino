d90c0a28d5d70f744ef13b06fee94884dd93ac9b
Lukas Stöckli <lukas.stoeckli@stud.unibas.ch>
2018-01-28 18:07:41 +0100

    get requests work, new design for website

--- src/main/serverTest.ino
+++ src/main/serverTest.ino
@@ -50,10 +50,12 @@ void serverLoop() {
         Serial.write(c); 
         request += c;
         
+        
         // if you've gotten to the end of the line (received a newline 
         // character) and the line is blank, the http request has ended, 
         // so you can send a reply 
         if (c == '\n' && currentLineIsBlank) { 
+          if (request.startsWith("GET /?text=")) break;
           // send a standard http response header 
           client.println("HTTP/1.1 200 OK"); 
           client.println("Content-Type: text/html"); 
@@ -65,15 +67,50 @@ void serverLoop() {
           client.println("<html lang=\"en\">"); 
           client.println("<head>"); 
           client.println("<meta charset=\"utf-8\">");
-          client.println("</head>"); 
+          client.println("</head>");
+          // css
+          client.println("<style>");
+          client.println("body { background-color: #d3d3d3;}");
+          client.println("input[type=text], select {");
+          client.println("width: 100%;");
+          client.println("padding: 12px 20px;");
+          client.println("margin: 8px 0;");
+          client.println("display: inline-block");
+          client.println("border: 1px solid #ccc");
+          client.println("border-radius: 4px;");
+          client.println("box-sizing: border-box;");
+          client.println("font-size: 150%;}");
+          client.println("input[type=submit], select {");
+          client.println("width: 100%;");
+          client.println("background-color: #4CAF50;");
+          client.println("color: white;");
+          client.println("padding: 14px 0;");
+          client.println("margin: 8px 0;");
+          client.println("border: none;");
+          client.println("border-radius: 4px;}");
+          client.println("input[type=submit]:hover {");
+          client.println("background-color: #45a049;}");
+          client.println("div {");
+          client.println("width: 50%;");
+          client.println("margin: auto;");
+          client.println("border-radius: 5px;");
+          client.println("background-color: #a2cd5a;");
+          client.println("padding: 20px;}");
+          client.println("</style>");
+          
+          // end of css
           client.println("<body>"); 
-          client.println("fuck yes, im the server");
-          client.print("<br/><br/>");
-          //client.println("<input>Enter text here</input>");
-          client.println("<form>Text:<br><input type=\"text\" name=\"text\">");
-          client.println("<a href=\"http://192.168.178.42\">");
-          client.println("<input type=\"submit\" value=\"Send text\"/>");
-          client.println("</a></form>");
+          client.println("<br/><br/><div>");
+          client.println("<h1>Welcome to the LED-Matrix webserver</h1>");
+          // input form
+          client.println("<div>");
+          client.println("<form>");
+          client.println("<label for=\"led\">Enter Text</label>");
+          client.println("<input type=\"text\" id=\"led\" name=\"text\">");
+          client.println("<a href=\"http://192.168.178.42\"><br/>");
+          client.println("<input type=\"submit\" value=\"Submit\"/>");
+          client.println("</a></form></div></div>");
+          // end of input form
           client.println("</body>"); 
           client.println("</html>"); 
           break; 
@@ -99,11 +136,22 @@ void serverLoop() {
 }
 
 void parseRequest(String *request) {
-  int startIndex = request->indexOf("=") +1; // 'GET /text='
-  int endIndex = request->indexOf("HTTP/1.1");
-  String message = request->substring(startIndex, endIndex);
+  int startIndex = request->indexOf("/"); // 'GET /?text=jahui HTTP/1.1\n'
+  if (!(request->charAt(startIndex + 1) == '?')) {
+    return;
+  }
+  startIndex += 7; // 'GET /?text=jahui HTTP/1.1\n'
+  int endIndex = request->indexOf("\n") - 10; // 'GET /?text=jahui HTTP/1.1\n'
+  char message[endIndex - startIndex];
+  // substring doesn't work
+  for(int i = 0; i < endIndex - startIndex; i++) {
+    message[i] = request->charAt(startIndex + i);
+  }
+  // black magic (it removes some weird characters, we don't
+  // know why either)
+  String str(message);
   Serial.println("------------");
   Serial.println(message);
-  writeText(message.c_str());
   Serial.println("------------");
+  writeText(message);
 }
